b7b42f07899b631dbe75d822185c1a6677d339cf
Gunther Hagleitner <hagleitn@masteronly-lm.(none)>
2010-09-23 00:47:02 -0700

    more cleanup

--- maze/src/MazeSolver.hs
+++ maze/src/MazeSolver.hs
@@ -9,8 +9,7 @@ import Control.Concurrent
 
 data Status = Status (Bool,Bool,Bool) | SUCCESS deriving (Show, Eq)
 data Direction = LEFT | RIGHT deriving (Show, Eq, Ord, Read)
-data Command = MOVE | TURN Direction | LOOK | NEW Int Int
-             | SHOW | RESET | SAVE | LOAD deriving (Show, Eq, Ord, Read)
+data Command = MOVE | TURN Direction | SHOW | RESET deriving (Show, Eq, Ord, Read)
 data Heading = N | W | S | E deriving (Show, Eq, Ord)             
 
 rotateHeading :: Heading -> Direction -> Heading
@@ -22,46 +21,25 @@ wallOrWay "wall" = True
 wallOrWay "corridor" = False
 
 instance Read Status where
-  readsPrec i s = do
-    (s1, r1) <- lex s
-    case s1 of
-      "\"Congratulations. You did it.\"" -> return (SUCCESS,r1)
-      _         -> do
-        (_, r1)  <- lex r1
-        (_, r1)  <- lex r1
-        (s1, r1) <- lex r1
-        b1 <- return $ wallOrWay s1
-        (_, r1)  <- lex r1
-        (_, r1)  <- lex r1
-        (_, r1)  <- lex r1
-        (_, r1)  <- lex r1
-        (_, r1)  <- lex r1
-        (s1, r1) <- lex r1
-        b2 <- return $ wallOrWay s1
-        (_, r1)  <- lex r1
-        (_, r1)  <- lex r1
-        (_, r1)  <- lex r1
-        (_, r1)  <- lex r1
-        (s1, r1)  <- lex r1
-        b3 <- return $ wallOrWay s1
-        return (Status (b1,b2,b3),"")
+  readsPrec i s =
+    let l@(l1:ls) = words s in
+      case l1 of
+        "\"Congratulations." -> [(SUCCESS,"")]
+        _ -> [(Status (wallOrWay (l!!3), wallOrWay (l!!8), wallOrWay (l!!12)),"")]
 
 executeRaw :: FilePath -> Command -> IO ()
 executeRaw fOut cmd = withFile fOut AppendMode (\h -> hPrint h cmd)
         
 executeRaw' :: FilePath -> [Command] -> IO ()
-executeRaw' fOut cs = forM_ cs (executeRaw fOut)
+executeRaw' fOut cs = forM_ cs $ executeRaw fOut
 
 execute :: FilePath -> FilePath -> Command -> IO Status
 execute fIn fOut command = do
   executeRaw fOut command
-  withFile fIn ReadMode getStatus
+  withFile fIn ReadMode $ liftM read . hGetLine
 
 execute' :: FilePath -> FilePath -> [Command] -> IO ()
-execute' fIn fOut cs = forM_ cs (execute fIn fOut)
-
-getStatus :: Handle -> IO Status
-getStatus hIn = liftM read $ hGetLine hIn
+execute' fIn fOut cs = forM_ cs $ execute fIn fOut
 
 incr :: Heading -> (Int,Int) -> (Int,Int)
 incr h (i,j) = case h of { N -> (i+1,j); S -> (i-1,j); W -> (i,j+1); E -> (i,j-1) }
@@ -70,13 +48,13 @@ tryAhead :: FilePath -> FilePath -> Status -> Heading -> [(Int,Int)] -> IO (Stat
 tryAhead _ _ s@(Status (_,True,_)) _ l = return (s,[],l)
 tryAhead fIn fOut _ h l@(l1:ls) = do
   s <- execute fIn fOut MOVE
-  (s2,c,l2) <- move s fIn fOut h ((incr h l1):l)
-  case s2 of
+  (s',c',l') <- move s fIn fOut h $ (incr h l1):l
+  case s' of
     SUCCESS -> return ()
     _ -> execute' fIn fOut [TURN LEFT,TURN LEFT,MOVE,TURN LEFT,TURN LEFT]
   putStr "."
   hFlush stdout
-  return (s2,MOVE:c,l2)
+  return (s',MOVE:c',l')
 
 tryTurn :: Direction -> FilePath -> FilePath -> Status -> Heading -> [(Int,Int)] -> IO (Status,[Command],[(Int,Int)])
 tryTurn LEFT _ _ s@(Status (True,_,_)) _ l = return (s,[],l)
@@ -84,22 +62,22 @@ tryTurn RIGHT _ _ s@(Status (_,_,True)) _ l = return (s,[],l)
 tryTurn dir fIn fOut status h l = do
   let rdir = case dir of { LEFT -> RIGHT; RIGHT -> LEFT }
   s <- execute fIn fOut $ TURN dir
-  (s2,c2,l2) <- tryAhead fIn fOut s (rotateHeading h dir) l
-  case s2 of
+  (s',c',l') <- tryAhead fIn fOut s (rotateHeading h dir) l
+  case s' of
     st@SUCCESS -> return st
     _ -> execute fIn fOut $ TURN rdir
-  return (s2,(TURN dir):c2,l2)
+  return (s',(TURN dir):c',l')
 
-move :: Status -> FilePath -> FilePath -> Heading -> [(Int,Int)] -> IO (Status, [Command], [(Int,Int)])
-move s@SUCCESS _ _ _ ls = do { putStrLn ""; return (s, [], ls) }
+move :: Status -> FilePath -> FilePath -> Heading -> [(Int,Int)] -> IO (Status,[Command],[(Int,Int)])
+move s@SUCCESS _ _ _ ls = return (s,[],ls)
 move status@(Status (b1,b2,b3)) fIn fOut h el@((i,j):ls)
-  | (i,j) `elem` ls = return (status, [], ls)
+  | (i,j) `elem` ls = return (status,[],ls)
   | otherwise = do
-      (s, c, l) <- tryTurn LEFT fIn fOut status h el
+      (s,c,l) <- tryTurn LEFT fIn fOut status h el
       case s of
         SUCCESS -> return (s,c,l)
         _ -> do
-          (s, c, l) <- tryAhead fIn fOut status h el
+          (s,c,l) <- tryAhead fIn fOut status h el
           case s of
             SUCCESS -> return (s,c,l)
             _ -> do
@@ -116,7 +94,7 @@ consume fIn silent = do
     else putStrLn l
   b <- hReady hIn
   hClose hIn
-  if (b) then consume fIn silent else return ()
+  if b then consume fIn silent else return ()
 
 main :: IO ()
 main = do
